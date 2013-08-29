c0afa5d312d170f24dfca311acd9864e95913d28
Jose Prado <cowlby@me.com>
2013-08-29 01:33:06 -0500

    Fixed remember_me capabilities

--- Security/Firewall/UsernamePasswordFormAuthenticationListener.php
+++ Security/Firewall/UsernamePasswordFormAuthenticationListener.php
@@ -35,9 +35,16 @@ class UsernamePasswordFormAuthenticationListener extends BaseAuthenticationListe
             return $authenticatedToken;
         }
 
+        if ($this->options['post_only']) {
+            $rememberMe = $request->request->get('_remember_me', false);
+        } else {
+            $rememberMe = $request->get('_remember_me', false);
+        }
+
         $request->getSession()->clear(SecurityContextInterface::AUTHENTICATION_ERROR);
 
         return new Response($this->templating->render('CowlbyDuoSecurityBundle:Authentication:duo.html.twig', array(
+            'remember_me' => $rememberMe,
             'duo_options' => json_encode(array(
                 'sig_request' => $this->duo->signRequest($authenticatedToken->getUser()),
                 'host' => $this->duo->getHost(),
