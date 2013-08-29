f3c867382f8c5835906550d857ea227b8dfd22cf
Jose Prado <cowlby@me.com>
2013-08-28 23:11:18 -0500

    Moved controllers to own config file

--- DependencyInjection/CowlbyDuoSecurityExtension.php
+++ DependencyInjection/CowlbyDuoSecurityExtension.php
@@ -31,5 +31,6 @@ class CowlbyDuoSecurityExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
+        $loader->load('controllers.xml');
     }
 }
